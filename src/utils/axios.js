e97cd09155982b664e965840d918e12a6a3b5bad
Nguyễn Thuận <nguyenthuan@Nguyens-MacBook-Pro-4.local>
2021-08-16 20:55:59 +0700

    delete netlify

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "https://shop3tclothing.herokuapp.com/",
+  baseURL: "https://shop3tclothing-server.herokuapp.com/",
 });
 axiosInstance.interceptors.request.use(
   (config) => new Promise((resolve) => setTimeout(() => resolve(config), 1000))
