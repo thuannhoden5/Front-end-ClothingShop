6dd3bb39a4a4b41cee5a2abca72665c8814d2cde
Nguyễn Thuận <nguyenthuan@Nguyens-MacBook-Pro-4.local>
2021-08-20 17:56:18 +0700

    added slidebar

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import Header from "./components/header/header.componet";
 import { Route, Switch } from "react-router-dom";
 import SignUp from "./components/auth/sign-up/sign-up.component";
 import ResetPassword from "./components/auth/reset-password/reset-password.component";
+import Slidebar from "./components/slidebar/Slidebar";
 function App() {
   return (
     <>
       <Header />
+      {/* <Header />
       <Switch>
         <Route exact path="/auth/signup" component={SignUp} />
         <Route exact path="/auth/signin" component={SignIn} />
         <Route exact path="/auth/resetpassword" component={ResetPassword} />
-      </Switch>
+      </Switch> */}
+      <Slidebar />
     </>
   );
 }
---
+++ src/components/slidebar/Slidebar.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import "./styles.scss";
+
+const Slidebar = () => {
+  return (
+    <div className="container">
+      <div
+        id="carouselExampleDark"
+        className="carousel carousel-dark slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active" data-bs-interval="10000">
+            <img
+              src="https://images.unsplash.com/photo-1607083206325-caf1edba7a0f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2556&q=80"
+              alt=""
+            />
+          </div>
+          <div className="carousel-item " data-bs-interval="2000">
+            <img
+              src="https://images.unsplash.com/photo-1540221652346-e5dd6b50f3e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1349&q=80"
+              alt=""
+            />
+          </div>
+          <div className="carousel-item" data-bs-interval="10000">
+            <div className="image-container">
+              <img
+                src="https://images.unsplash.com/photo-1619166719123-471cee9ce91e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleDark"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleDark"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+    // <div className="w3-display-container mySlides">
+    //   <img
+    //     src="https://images.unsplash.com/photo-1441986300917-64674bd600d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80"
+    //     alt=""
+    //   />
+    //   <div class="w3-display-bottomleft w3-container w3-padding-16 w3-black">
+    //     French Alps
+    //   </div>
+    // </div>
+    // <div className="w3-content w3-section">
+    //   <img className="mySlides" src="" />
+    //   <img className="mySlides" src="" />
+    //   <img className="mySlides" src="" />
+    // </div>
+  );
+};
+export default Slidebar;
