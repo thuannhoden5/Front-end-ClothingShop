cf2db7dda71c6d2af30b80ba0775883a90992038
thuannhoden5 <thuannhoden5@gmail.com>
2021-10-23 18:41:05 +0700

    chnage picture

--- src/components/picture/picture.components.jsx
+++ src/components/picture/picture.components.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const Picture = () => {
   return (
-    <div>
+    <div className="card" style={{ width: 250, height: 280 }}>
       <img
         src="https://images.unsplash.com/photo-1534353436294-0dbd4bdac845?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
         alt=""
         className="img-thumbnail"
         style={{
-          width: 180,
-          height: 200,
+          width: 250,
+          height: 250,
           backgroundSize: "cover",
         }}
       />
-      <h6 className="card-title"></h6>
+      <h5 className="card-title text-center">hello</h5>
     </div>
   );
 };
